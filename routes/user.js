a1e77cbcf866a093de26b119822feb4bc921c4b5
webguyakash <akash.kumar@araiinfotech.com>
2025-03-26 13:55:59 +0530

    manual-auth: created a authentication

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const usersConrtoller = require('../controllers/users_controller');
+const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersConrtoller.profile);
-router.get('/sign_up', usersConrtoller.signUp);
-router.get('/sign_in', usersConrtoller.signIn);
+// Profile Route
+router.get('/profile', usersController.profile);
 
-router.post('/create', usersConrtoller.create);
+// Sign Up & Sign In Routes
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
+
+// Create New User & Create Session Routes
+router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 module.exports = router;
